22722c28838fd73a22266a3eb3e9892b1d7cf67f
Srikrishan Bhatt <srikrishnabhatt@gmail.com>
2026-06-07 14:44:45 +0530

    Created electron app

---
+++ electron/main.cjs
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
